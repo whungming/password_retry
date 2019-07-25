48636d3bdd35bd06f0c69421456401ccd3db3ed5
Hung-Ming Wu <whungming@yahoo.com>
2019-07-25 12:37:50 +0800

    advance version

--- password.py
+++ password.py
@@ -1,11 +1,15 @@
 password = 'a123456'
 i = 3 # the chance left
+
 while i > 0:
+	i = i - 1
 	pwd = input('enter pasword: ')
 	if pwd == password:
 		print('sign in success!')
 		break
 	else:
-		i = i - 1
-		print('wrong password! only has', i, 'chance left')
-		
+	    print('wrong password!')
+	    if i > 0:
+		    print('only has', i, 'chance left')
+	    else:
+		    print('no more chance to sign in')
